5098773081358cd07c177a194d8283d7ed6a9d5d
eka-dyah <eka.dya1205@gmail.com>
2020-09-18 21:47:26 +0700

    alarm url change to https

--- src/components/Ring.js
+++ src/components/Ring.js
@@ -4,7 +4,7 @@ const Ring = forwardRef((props, ref) => {
     
     return (
         <audio
-            src="http://onj3.andrelouis.com/phonetones/unzipped/Palm-Treo800W/Alarm5.wav"
+            src="https://onj3.andrelouis.com/phonetones/unzipped/Palm-Treo800W/Alarm5.wav"
             ref={ref}
         ></audio>
     );
